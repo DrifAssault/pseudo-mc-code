621fb759e2231355ceef2b3f1cee03a185895eeb
DrifAssault <69107664+DrifAssault@users.noreply.github.com>
2021-03-11 19:48:41 +0700

    commitance.

--- cactus.py
+++ cactus.py
@@ -1,22 +1,33 @@
 # psuedo-code, random in Java is nextInt, 1 LCG call
-# random(3) can only generate 0, 1, 2
-cactus_seed = int(input("Cactus seed: "))
+# nextInt(3) can only generate 0, 1, 2
+
+seed = int(input("Cactus seed: "))
+
+def nextInt(bound):
+    seed = (25214903917 * seed + 11) % (2**48)
+    if (bound == None) {return seed >> 16}
+    elif (bound != None){
+
+    }
+
+i = int(input("x chunk coord?"))
+j = int(input("y chunk coord?"))
 # i, j is seemingly generated via
 k = 16*i
 l = 16*j
 for sth in range(0, ):
-    x = k + cactus_seed.random(16) + 8;
-    y = cactus_seed.random(128);
-    z = l + cactus_seed.random(16) + 8;
-    new_cactus = generate_cactus(cactus_seed, x, y, z)
+    x = k + seed.nextInt(16) + 8;
+    y = seed.nextInt(128);
+    z = l + seed.nextInt(16) + 8;
+    new_cactus = generate_cactus(seed, x, y, z)
 
 def generate_cactus(seed_random, x, y, z):
     for l in range(0, 10):
-        attempt_x = x + random(8) - random(8);
-        attempt_y = y + random(4) - random(4);
-        attempt_z = z + random(8) - random(8);
+        attempt_x = x + nextInt(8) - nextInt(8);
+        attempt_y = y + nextInt(4) - nextInt(4);
+        attempt_z = z + nextInt(8) - nextInt(8);
         if (not air_block(attempt_x, attempt_y, attempt_z)) pass
-        height = 1 + random(random(3) + 1)
+        height = 1 + nextInt(nextInt(3) + 1)
         for y_offset in range(0, height):
             # canBlockStay only valid with normal cactus conditions (on sand, no obstructions, etc.)
             if (canBlockStay(attempt_x, attempt_y + y_offset, attempt_z))
