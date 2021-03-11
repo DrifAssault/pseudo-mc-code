787ecb367dcf2aba94745bddd8d53dc4fc414739
DrifAssault <69107664+DrifAssault@users.noreply.github.com>
2021-03-11 20:33:13 +0700

    h

--- cactus.py
+++ cactus.py
@@ -1,15 +1,29 @@
 # psuedo-code, random in Java is nextInt, 1 LCG call
 # nextInt(3) can only generate 0, 1, 2
 import math
+def mod(num, a):
+    res = 0
+    for i in range(0, len(num)):
+        res = (res * 10 + int(num[i])) % a;
+    return res
+
 seed = int(input("Cactus seed: "))
-
-def nextInt(bound):
-    seed = (25214903917 * seed + 11) % (2**48)
-    if (bound == None): return seed >> 16
-    elif (math.ceil(Log2(bound)) == math.floor(Log2(bound))): return seed >> (31 - math.floor(Log2(bound)))
-
+bound = int(input("Bound (if you wanna have no bound, put 0): "))
 i = int(input("x chunk coord?"))
 j = int(input("y chunk coord?"))
+
+def nextInt(bound):
+    seed = mod(str((25214903917 * seed) + 11), (2**48))
+    if (bound == 0):
+        out = int(seed)
+        out << (48 - math.ceil(math.log(out, 2)))
+        out = out >> 16
+    elif (math.ceil(math.log(bound, 2)) == math.floor(math.log(bound, 2))):
+        out = int(seed)
+        out << (48 - math.ceil(math.log(out, 2)))
+        out = out >> (48 - math.floor(math.log(bound, 2)))
+    return out
+
 # i, j is seemingly chunk coord
 k = 16*i
 l = 16*j
